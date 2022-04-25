22562e3ff6154c70b069f24aa09a24bdb20b7286
victor kirui <vkkiprop@gmail.com>
2022-04-25 14:27:59 +0300

    Create product component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 // Feature 1
 import "./index.css";
+import React, { Component } from "react";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
-  return (
-    <div className="App">
-      <header>header</header>
-      <main>main</main>
-      <footer>footer</footer>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>header</header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">sidebar</div>
+          </div>
+        </main>
+        <footer>footer</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
